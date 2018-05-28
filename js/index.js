0e3c6f944782be652c8c21bdd993838cc19ad797
Noble <officiallynya@gmail.com>
2018-05-28 02:01:03 -0400

    initial

---
+++ js/index.js
@@ -0,0 +1,16 @@
+AOS.init({
+  duration: 1200,
+})
+
+
+
+
+
+$(document).ready(function() {
+	
+	setTimeout(function(){
+		$('body').addClass('loaded');
+		// $('h1').css('color','#222222');
+	}, 3600);
+	
+});
